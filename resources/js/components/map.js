0d3865bcc7349f49e89319a95babd542927d1aad
Rayounette <murielarisoaran@gmail.com>
2023-08-06 16:42:20 +0300

    init

--- resources/js/components/map.js
+++ resources/js/components/map.js
@@ -1,64 +1,43 @@
 import Map from "ol/Map.js";
 import View from "ol/View.js";
-import TileLayer from "ol/layer/Tile.js";
-import VectorSource from "ol/source/Vector";
-import VectorLayer from "ol/layer/Vector";
+import {
+    Tile as TileLayer
+} from 'ol/layer.js';
 import OSM from "ol/source/OSM.js";
-import { Style, Fill, Stroke, Circle, Text } from "ol/style.js";
-import GeoJSON from "ol/format/GeoJSON";
 import Overlay from "ol/Overlay.js";
+import TileWMS from 'ol/source/TileWMS.js';
+import Feature from "ol/Feature";
+import Point from 'ol/geom/Point.js';
 
 document.addEventListener("alpine:init", () => {
     Alpine.data("map", function () {
         return {
             legendOpened: false,
             map: {},
+            activeTab: 'legend',
             initComponent() {
-                let paramsObj = {
-                    servive: "WFS",
-                    version: "2.0.0",
-                    request: "GetFeature",
-                    outputFormat: "application/json",
-                    crs: "EPSG:4326",
-                    srsName: "EPSG:4326",
-                };
-
-                const baseUrl = "http://localhost:8081/geoserver/wfs?";
-
-                paramsObj.typeName = "laravelgis:monuments";
-                let urlParams = new URLSearchParams(paramsObj);
-
-                let monumentsLayer = new VectorLayer({
-                    source: new VectorSource({
-                        format: new GeoJSON(),
-                        url: baseUrl + urlParams.toString(),
+                let monumentsLayer = new TileLayer({
+                    source: new TileWMS({
+                        url: 'http://localhost:8081/geoserver/wms',
+                        params: {
+                            'LAYERS': 'laravelgis:monuments',
+                            'TILED': true
+                        },
+                        serverType: 'geoserver',
                     }),
-                    style: this.monumentsStyleFunction,
                     label: 'Monuments',
                 });
 
-                paramsObj.typeName = "laravelgis:world-administrative-boundaries";
-                urlParams = new URLSearchParams(paramsObj);
-
-                let worldAdministrativeBoundariesLayer = new VectorLayer({
-                    source: new VectorSource({
-                        format: new GeoJSON(),
-                        url: baseUrl + urlParams.toString(),
+                let batiLayer = new TileLayer({
+                    source: new TileWMS({
+                        url: 'http://localhost:8081/geoserver/wms',
+                        params: {
+                            'LAYERS': 'laravelgis:bati',
+                            'TILED': true
+                        },
+                        serverType: 'geoserver',
                     }),
-                    style: this.worldAdministrativeBoundariesStyleFunction,
-                    label: 'World Administrative Boundaries',
-                });
-
-                paramsObj.typeName = "laravelgis:bati";
-                urlParams = new URLSearchParams(paramsObj);
-
-                let worldRiversLayer = new VectorLayer({
-                    source: new VectorSource({
-                        format: new GeoJSON(),
-                        url: baseUrl + urlParams.toString(),
-                    }),
-                    style: this.worldRiversStyleFunction,
-                    label: 'World Rivers',
+                    label: 'Batiments',
                 });
 
                 this.map = new Map({
@@ -68,14 +47,13 @@ document.addEventListener("alpine:init", () => {
                             source: new OSM(),
                             label: 'OpenStreetMap',
                         }),
-                        worldAdministrativeBoundariesLayer,
-                        worldRiversLayer,
+                        batiLayer,
                         monumentsLayer
                     ],
                     view: new View({
                         projection: "EPSG:4326",
-                        center: [0, 0],
-                        zoom: 2,
+                        center: [-78.2161, -0.7022],
+                        zoom: 8,
                     }),
                     overlays: [
                         new Overlay({
@@ -95,113 +73,59 @@ document.addEventListener("alpine:init", () => {
                     overlay.setPosition(undefined)
                     this.$refs.popupContent.innerHTML = ''
 
-                    this.map.forEachFeatureAtPixel(
-                        event.pixel,
-                        (feature, layer) => {
-                            if (layer.get('label') === 'Monuments' && feature) {
-                                this.gotoFeature(feature)
+                    const viewResolution = /** @type {number} */ (event.map.getView().getResolution())
 
-                                let content =
-                                    '<h4 class="text-gray-500 font-bold">' +
-                                    feature.get('name') +
-                                    '</h4>'
+                    const url = monumentsLayer.getSource().getFeatureInfoUrl(
+                        event.coordinate,
+                        viewResolution,
+                        'EPSG:4326', {
+                            'INFO_FORMAT': 'application/json'
+                        })
 
-                                content +=
-                                    '<img src="' +
-                                    feature.get('image') +
-                                    '" class="mt-2 w-full max-h-[200px] rounded-md shadow-md object-contain overflow-clip">'
+                    if (url) {
+                        fetch(url)
+                            .then((response) => response.json())
+                            .then((json) => {
+                                if (json.features.length > 0) {
+                                    let jsonFeature = json.features[0]
 
-                                this.$refs.popupContent.innerHTML = content
+                                    let feature = new Feature({
+                                        geometry: new Point(jsonFeature.geometry.coordinates),
+                                        name: jsonFeature.properties.name,
+                                        image: jsonFeature.properties.image
+                                    })
 
-                                setTimeout(() => {
-                                    overlay.setPosition(
-                                        feature.getGeometry().getCoordinates()
-                                    );
-                                }, 500)
+                                    this.gotoFeature(feature)
 
-                                return
-                            }
-                        },
-                        {
-                            hitTolerance: 5,
-                        }
-                    );
+                                    let content =
+                                        '<h4 class="text-gray-500 font-bold">' +
+                                        feature.get('name') +
+                                        '</h4>'
+
+                                    content +=
+                                        '<img src="' +
+                                        feature.get('image') +
+                                        '" class="mt-2 w-full max-h-[200px] rounded-md shadow-md object-contain overflow-clip">'
+
+                                    this.$refs.popupContent.innerHTML = content
+
+                                    setTimeout(() => {
+                                        overlay.setPosition(
+                                            feature.getGeometry().getCoordinates()
+                                        );
+                                    }, 500)
+
+                                    return
+                                }
+                            });
+                    }
+
                 });
             },
             closePopup() {
                 let overlay = this.map.getOverlayById('info')
                 overlay.setPosition(undefined)
                 this.$refs.popupContent.innerHTML = ''
-            },
-            monumentsStyleFunction(feature, resolution) {
-                return new Style({
-                    image: new Circle({
-                        radius: 4,
-                        fill: new Fill({
-                            color: "rgba(0, 255, 255, 1)",
-                        }),
-                        stroke: new Stroke({
-                            color: "rgba(192, 192, 192, 1)",
-                            width: 2,
-                        }),
-                    }),
-                    text: new Text({
-                        font: "12px sans-serif",
-                        textAlign: "left",
-                        text: feature.get("name"),
-                        offsetY: -15,
-                        offsetX: 5,
-                        backgroundFill: new Fill({
-                            color: "rgba(255, 255, 255, 0.5)",
-                        }),
-                        backgroundStroke: new Stroke({
-                            color: "rgba(227, 227, 227, 1)",
-                        }),
-                        padding: [5, 2, 2, 5],
-                    }),
-                });
-            },
-            worldAdministrativeBoundariesStyleFunction(feature, resolution) {
-                return new Style({
-                    fill: new Fill({
-                        color: "rgba(125, 125, 125, 0.1)",
-                    }),
-                    stroke: new Stroke({
-                        color: "rgba(125, 125, 125, 1)",
-                        width: 2,
-                    }),
-                    text: new Text({
-                        font: "16px serif bold",
-                        text: feature.get("name"),
-                        fill: new Fill({
-                            color: "rgba(32, 32, 32, 1)",
-                        }),
-                    }),
-                });
-            },
-            worldRiversStyleFunction(feature, resolution) {
-                let text;
-                let width = 2;
-
-                if(resolution < 0.002){
-                    text = new Text({
-                        font: "20px serif",
-                        text: feature.get("river_map"),
-                        fill: new Fill({
-                            color: "rgba(0, 0, 255, 1)",
-                        }),
-                    });
-
-                    width = 4;
-                }
-
-                return new Style({
-                    stroke: new Stroke({
-                        color: "rgba(0, 0, 255, 1)",
-                        width: width,
-                    }),
-                    text: text,
-                });
             },
             gotoFeature(feature) {
                 this.map.getView().animate({
@@ -210,6 +134,18 @@ document.addEventListener("alpine:init", () => {
                     duration: 500,
                 });
             },
+            hasLegend(layer) {
+                return layer.getSource() instanceof TileWMS
+            },
+            legendUrl(layer) {
+                if (this.hasLegend(layer)) {
+                    return layer
+                        .getSource()
+                        .getLegendUrl(this.map.getView().getResolution(), {
+                            LEGEND_OPTIONS: 'forceLabels:on'
+                        })
+                }
+            }
         };
     });
 });
--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -2,7 +2,7 @@
     <div x-ref="map" class="relative h-[75vh] overflow-clip rounded-md border border-slate-300 shadow-lg">
         <div class="absolute top-2 right-8 z-10 rounded-md bg-white bg-opacity-75">
             <div class="ol-unselectable ol-control">
-                <button x-on:click.prevent="legendOpened = ! legendOpened" title="Open/Close legend"
+                <button x-on:click.prevent="legendOpened = ! legendOpened" title="Open/Close details"
                     class="absolute inset-0 flex items-center justify-center">
                     <!-- Heroicon name: outline/globe -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 pl-0.5" fill="none" viewBox="0 0 24 24"
@@ -19,38 +19,58 @@
             x-transition:leave="transition-opacity duration-300" x-transition:leave-start="opacity-100"
             x-transition:leave-end="opacity-0"
             class="absolute right-0 top-16 left-2 bottom-2 z-10 max-w-sm rounded-md border border-slate-300 bg-white bg-opacity-50 shadow-sm">
-            <div class="absolute inset-1 rounded-md bg-white bg-opacity-75 p-2">
-                <div class="flex items-start justify-between">
-                    <h3 class="text-lg font-medium text-slate-700">Legend</h3>
+            <div class="absolute inset-1 overflow-y-auto rounded-md bg-white bg-opacity-75 p-2 pt-1">
+                <div class="flex items-center justify-between pr-1">
+                    <div class="flex justify-start space-x-3">
+                        <h3 x-on:click.prevend="activeTab = 'legend'" class="cursor-pointer text-slate-700"
+                            x-bind:class="activeTab === 'legend' && 'font-bold'">Legend</h3>
+                        <h3 x-on:click.prevend="activeTab = 'monuments'" class="cursor-pointer text-slate-700"
+                            x-bind:class="activeTab === 'monuments' && 'font-bold'">Monuments</h3>
+                    </div>
                     <button x-on:click.prevent="legendOpened = false"
-                        class="text-2xl font-black text-slate-400 transition hover:text-[#3369A1] focus:text-[#3369A1] focus:outline-none">&times;</button>
+                        class="mb-1 text-2xl font-black text-slate-400 transition hover:text-[#3369A1] focus:text-[#3369A1] focus:outline-none">&times;</button>
                 </div>
-                <ul class="mt-2 space-y-1 rounded-md border border-slate-300 bg-white p-2">
+                <ul x-show="activeTab === 'legend'" x-transition:enter="transition-opacity duration-150"
+                    x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+                    x-transition:leave="transition-opacity duration-150" x-transition:leave-start="opacity-100"
+                    x-transition:leave-end="opacity-0"
+                    class="mt-2 p-1 space-y-1 rounded-md border border-slate-300 bg-white">
                     <template x-for="(layer, index) in map.getAllLayers().reverse()" :key="index">
-                        <li class="flex items-center px-2 py-1">
-                            <div x-id="['legend-range']" class="w-full">
-                                <label x-bind:for="$id('legend-range')" class="flex items-center">
-                                    <span class="text-sm text-slate-600" x-text="layer.get('label')"></span>
-                                </label>
-                                <div class="mt-1 text-sm text-slate-600">
-                                    <input class="w-full accent-[#3369A1]" type="range" min="0" max="1" step="0.01"
-                                        x-bind:id="$id('legend-range')" x-bind:value="layer.getOpacity()"
+                        <li class="flex items-center p-0.5">
+                            <div x-id="['legend-range']" class="w-full rounded-md border border-gray-300 px-2 py-1">
+                                <div class="space-y-1">
+                                    <label x-bind:for="$id('legend-range')" class="flex items-center">
+                                        <span class="text-sm text-slate-600" x-text="layer.get('label')"></span>
+                                    </label>
+                                    <div x-show="hasLegend(layer)">
+                                        <img x-bind:src="legendUrl(layer)" alt="Legend">
+                                    </div>
+                                </div>
+                                <div class="mt-2 text-sm text-slate-600">
+                                    <input class="w-full accent-[#3369A1]" type="range" min="0" max="1"
+                                        step="0.01" x-bind:id="$id('legend-range')" x-bind:value="layer.getOpacity()"
                                         x-on:change="layer.setOpacity(Number($event.target.value))">
                                 </div>
                             </div>
                         </li>
                     </template>
                 </ul>
+                <div x-show="activeTab === 'monuments'" x-transition:enter="transition-opacity duration-150"
+                    x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+                    x-transition:leave="transition-opacity duration-150" x-transition:leave-start="opacity-100"
+                    x-transition:leave-end="opacity-0" class="mt-2 p-1 rounded-md border border-slate-300 bg-white">
+                    Monuments
+                </div>
             </div>
         </div>
         <div x-cloak x-ref="popup" class="ol-popup ol-control transition">
-            <div class="p-2 m-0.5 bg-white rounded-md">
+            <div class="m-0.5 rounded-md bg-white p-2">
                 <div class="flex justify-between">
                     <h3 class="text-xs font-medium text-slate-400">Monument</h3>
                     <a href="#" title="Close" x-on:click.prevent="closePopup"
                         class="-mt-1 font-black text-slate-400 transition hover:text-slate-600 focus:text-slate-600 focus:outline-none">&times;</a>
                 </div>
-                <div x-ref="popupContent" class="mt-2 overflow-y-auto min-h-[200px]"></div>
+                <div x-ref="popupContent" class="mt-2 min-h-[200px] overflow-y-auto"></div>
             </div>
         </div>
     </div>
